4fda2aa4f7fc0567a032dc829dda27c66a59ad65
rdragan <rdragan@student.42heilbronn.de>
2023-11-15 13:12:54 +0100

    rename form

---
+++ CPP05/ex02/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 06:48:45 by rdragan           #+#    #+#             */
+/*   Updated: 2023/11/15 12:58:44 by rdragan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class AForm;
+
+class	Bureaucrat
+{
+public:
+	Bureaucrat(void);
+	Bureaucrat(std::string name);
+	Bureaucrat(std::string name, int grade);
+	// Bureaucrat(const Bureaucrat& obj);
+	// Bureaucrat&	operator=(const Bureaucrat& obj);
+	~Bureaucrat(void);
+	std::string	getName(void);
+	int			getGrade(void);
+	void		incrementGrade(void);
+	void		decrementGrade(void);
+	void		signForm(AForm& form);
+	class	GradeTooHighException : public std::exception
+	{
+	public:
+		char const*	what(void) const throw();
+	};
+	class	GradeTooLowException : public std::exception
+	{
+	public:
+		char const*	what(void) const throw();
+	};
+private:
+	std::string	_name;
+	// grade must be between [150-1] 150:low 1:high
+	int	_grade;
+};
+
+std::ostream&	operator<<(std::ostream& COUT, Bureaucrat&	bureaucrat);
+
+#endif
