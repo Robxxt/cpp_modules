c9e93a6149a7a17a830b8134797a7179ddfc4308
rdragan <rdragan@student.42heilbronn.de>
2023-12-10 13:31:08 +0100

    finish ex00

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 13:00:28 by rdragan           #+#    #+#             */
+/*   Updated: 2023/12/10 13:16:31 by rdragan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T>
+void	swap(T* a, T* b);
+template <typename T>
+T		min(T a, T b);
+template <typename T>
+T		max(T a, T b);
+#endif
