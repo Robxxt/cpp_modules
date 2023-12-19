20177e786583741458fb454bc4dad2951a0deb13
rdragan <rdragan@student.42heilbronn.de>
2023-12-19 15:15:14 +0100

    finish

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:31:58 by rdragan           #+#    #+#             */
-/*   Updated: 2023/12/19 14:57:35 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/12/19 15:14:35 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <stdexcept>
 # include <cmath>
-# include <iostream> // TO REMOVE
+# include <iostream>
 
 class	Span
 {
@@ -32,7 +32,7 @@ class	Span
 		Span&	operator=(const Span& s);
 		~Span();
 		void	addNumber(int n);
-		void	print() const; // TO REMOVE
+		void	print() const;
 		int		shortestSpan() const;
 		int		longestSpan() const;
 		void	addRange(unsigned int end);
--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,14 +6,11 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:28:47 by rdragan           #+#    #+#             */
-/*   Updated: 2023/12/19 15:09:24 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/12/19 15:14:45 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
 
 void	testAssignmentOverload()
 {
@@ -46,25 +43,26 @@ void	testPdfCase()
 	std::cout << "--------------------------------" << std::endl;
 }
 
-void	testAddRange()
+void	testShortAndLongSpan()
 {
-	std::cout << "testAddRange" << std::endl;
+	std::cout << "testShortAndLongSpan" << std::endl;
 
 	std::cout << "--------------------------------" << std::endl;
-	Span	a(5);
+	Span	a(10000);
 	std::cout << "Before calling the function" << std::endl;
 	a.print();
 	std::cout << std::endl;
 	std::cout << "After calling the function" << std::endl;
-	a.addRange(5);
-	a.print();
+	a.addRange(10000);
+	std::cout << "shortestSpan: " << a.shortestSpan() << std::endl;
+	std::cout << "longestSpan: " << a.longestSpan() << std::endl;
 	std::cout << "--------------------------------" << std::endl;
 }
 
 int	main(void)
 {
 	testPdfCase();
-	testAddRange();
+	testShortAndLongSpan();
 	testAssignmentOverload();
 	return (0);
 }
